b62dd2bd969384a149eb7df8ffe80af17bfe454a
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-02-04 21:40:47 +0200

    fix: improve docstring consistency and grammar in HelprAction class

--- .dagger/src/helpr_action/main.py
+++ .dagger/src/helpr_action/main.py
@@ -4,14 +4,16 @@ from dagger import dag, function, object_type
 
 @object_type
 class HelprAction:
+    """Dagger object type for HelprAction."""
+
     @function
     def container_echo(self, string_arg: str) -> dagger.Container:
-        """Returns a container that echoes whatever string argument is provided"""
+        """Return a container that echoes whatever string argument is provided."""
         return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
 
     @function
     async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
-        """Returns lines that match a pattern in the files of the provided Directory"""
+        """Return lines that match a pattern in the files of the provided Directory."""
         return await (
             dag.container()
             .from_("alpine:latest")
